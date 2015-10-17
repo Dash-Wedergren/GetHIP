c8fd611ed452e183a53cc756ff5c4bbcfeaac45c
Dash-Wedergren <dashwedergren@gmail.com>
2015-10-17 19:56:54 +0000

    finished assignment3

--- assignment3/Sort.java
+++ assignment3/Sort.java
@@ -3,33 +3,37 @@ import java.util.*;
 public class Sort {
     
     public static void main(String[] args){
-        List<Shape> sorted = new ArrayList<Shape>();
-        List unsorted = new ArrayList();
+        List<Double> unsorted = new ArrayList<Double>();
 	    List<Shape> shapes = new ArrayList<Shape>();
-	    List position = new ArrayList();
         Integer counter = 0;
-        Integer current;
-        shapes.add(new Circle(6));
-	    shapes.add(new Triangle(6, 4));
-	    shapes.add(new Rectangle(8, 3));
+        shapes.add(new Rectangle(8, 3));
 	    shapes.add(new Rhombus(6, 5));
+        shapes.add(new Triangle(6, 2));
+	    shapes.add(new Triangle(6, 4));
 	    shapes.add(new Trapezoid(4, 10, 5));
-        for(Integer i = 0;i<shapes.size();i++){
-            position.add(i);
-        }
+        shapes.add(new Circle(2));
+        shapes.add(new Triangle(1,1));
+        
         for(Shape s : shapes){
             unsorted.add(s.getArea());
-            //position.add()
         }
+        
+        System.out.println(unsorted);
+        
+        //Insertion Sort        
         for(Integer i = 0;i<unsorted.size();i++){
-            current = unsorted.get(i);
             counter = i;
-            while(counter > 0 && unsorted.get(counter-1) > current){
+            double current = unsorted.get(i);
+            //while position is greater than 0 check if current value is greater than previous element
+            while((counter > 0) && (unsorted.get(counter-1) > current)){
+                //shift other numbers then subtract from counter
                 unsorted.set(counter, unsorted.get(counter-1));
                 counter--;
             }
+            //set element to position found
             unsorted.set(counter, current);
         };
+        
         System.out.println(unsorted);
     }
 }
